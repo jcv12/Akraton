3e37e097ae183be4e22704545647cf2e6c829315
JCV12 <john.c.vayianos@gmail.com>
2025-11-27 10:27:14 -0500

    Added the config, practice with the browser type and will make some mroe laer

--- akraton/Program.cs
+++ akraton/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 
 // Starter, going to make it more dynamica
 class Program
@@ -10,17 +12,39 @@ class Program
     [DllImport("user32.dll")]
     static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 
-    static void Main(string[] args)
-
-
+    public class AppSettings
     {
+        public string BrowserType { get; set; }
+
+    }
+
+    static void Main(string[] args)
+    {
+        //Lets grab our config file we gots
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+        IConfiguration config = builder.Build();
+
+        string browserType = config["AppSettings:BrowserType"];
+
+
+        //Push it into the Class so we can use it later
+        var settings = config
+            .GetSection("AppSettings")
+            .Get<AppSettings>();
+
+
+
         // Open up youtube
         Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "--new-window https://www.youtube.com/watch?v=81tWAoRcngo&list=RDx3hoYr2dZfY&index=3");
 
         Thread.Sleep(2000);
 
         // Grab all of the processes that are edge and youtube then lets go move it to my lfet side
-        Process[] procs = Process.GetProcessesByName("msedge");
+        Process[] procs = Process.GetProcessesByName(browserType);
         foreach (var p in procs)
         {
             if (!string.IsNullOrEmpty(p.MainWindowTitle) &&
